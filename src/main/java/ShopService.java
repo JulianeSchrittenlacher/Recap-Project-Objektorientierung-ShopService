0d9839977c343e4be74a59ee7a0605c2964d4914
julianeschrittenlacher <julianeschrittenlacher@gmail.com>
2024-06-12 13:56:53 +0200

    modify addOrder method so that an exception is thrown if the product does not exist.

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -8,18 +9,29 @@ public class ShopService {
     private OrderRepo orderRepo = new OrderMapRepo();
 
 
-    public Order addOrder(List<String> productIds) {
+    public Order addOrder(List<String> productIds) throws IdNotFoundException{
         List<Product> products = new ArrayList<>();
         for (String productId : productIds) {
-            Product productToOrder = productRepo.getProductById(productId);
-            if (productToOrder == null) {
-                System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
-                return null;
+            try {
+                Optional<Product> productToOrder = productRepo.getProductById(productId);
+                if (productToOrder.isPresent()) {
+                    products.add(productToOrder.get());
+                } else {
+                    throw new IdNotFoundException(productId);
+                }
+            } catch (IdNotFoundException e) {
+                throw e;
             }
-            products.add(productToOrder);
         }
+//        for (String productId : productIds) {
+//            Product productToOrder = productRepo.getProductById(productId);
+//            if (productToOrder == null) {
+//                System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
+//                return null;
+//            }
+//            products.add(productToOrder);
+//        }
         Order newOrder = new Order(UUID.randomUUID().toString(), products,OrderStatus.PROCESSING);
-
         return orderRepo.addOrder(newOrder);
     }
 
